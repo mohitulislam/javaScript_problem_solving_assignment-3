e9e973c4548fc5c0208d6ca9e846854668b14694
mohitulislam <md.mohitulislam@live.com>
2021-02-07 01:00:26 +0600

    add assignment_3

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 //  https://github.com/mohitulislam/javaScript_problem_solving_assignment-3
 
-// Problemt-1 = convert Kilometer to Meter
+// Problem-1 = convert Kilometer to Meter
 function kilometerToMeter(inputKilometer) {
     
     if(inputKilometer < 0){
